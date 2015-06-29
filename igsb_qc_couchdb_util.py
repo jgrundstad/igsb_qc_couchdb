d731fdc9cb0225600214f47300e41402a3e00c0a
Jason Grundstad <jgrundstad@gmail.com>
2015-06-29 15:11:04 -0500

    add and delete fields, including coverage stats

--- igsb_qc_couchdb_util.py
+++ igsb_qc_couchdb_util.py
@@ -14,15 +14,22 @@ Options:
     -A add_fields        comma-delimited list of field:value pairs
 """
 __author__ = 'A. Jason Grundstad'
+import ast
 from docopt import docopt
 from couchdbreq import Server
 import requests
 import requests.auth
 import pprint
+import json
+import sys
 
 
-def connect_to_db(server_name, credentials):
-    """ Returns server object used to access db
+def connect_to_server(server_name, credentials):
+    """
+    Connect to provided server with the given credentials
+    :param server_name: e.g. 'http://127.0.0.1:5984'
+    :param credentials: e.g. 'name:password'
+    :return: couchdbreq server object
     """
     u = credentials.split(':')[0]
     p = credentials.split(':')[1]
@@ -34,15 +41,28 @@ def connect_to_db(server_name, credentials):
     print "Connected!\n{}".format(server.get_info())
     return server
 
+
 def delete_document(db, doc):
-    """ Careful
+    """
+    This does what you think it does.
+    :param db: CouchDB obj
+    :param doc: document
+    :return:
     """
     print "Deleting document {}".format(doc['_id'])
-    db.delete_doc(doc)
+    try:
+        db.delete_doc(doc)
+    except Exception, e:
+        print >>sys, "Error when deleting document: {}".format(doc)
+        raise
 
 
 def view_docs_by_bnid(db, bnid):
-    """ Access the 'all' view.  Returns all docs containing Bionimbus_id
+    """
+    Access the 'all' view.  Returns all docs containing the given Bionimbus_id
+    :param db: CouchDB obj
+    :param bnid: Bionimbus ID
+    :return:
     """
     rows = db.view('igsb_qc/all', key=bnid, descending=True)
     pp = pprint.PrettyPrinter(indent=4)
@@ -51,9 +71,13 @@ def view_docs_by_bnid(db, bnid):
         print row['value']['_id']
     return rows
 
+
 def find_duplicate_docs(rows, db):
     """
     find docs that match on 'fields', disregard analysis date fields
+    :param rows: set of documents (returned from a view)
+    :param db: CouchDB obj
+    :return:
     """
     fields = ('Bionimbus_id', 'Date', 'Machine', 'Run', 'BarCode', 'Lane',
               'aligned_bp_ot', 'total_reads', 'target_size')
@@ -75,6 +99,56 @@ def find_duplicate_docs(rows, db):
     print "Encountered {} documents that appear to be duplicates".format(len(dup_documents))
     return dup_documents
 
+
+def delete_field(db, doc, field, subfield=None):
+    """
+    Delete a field from a given CouchDB document
+    :param db: CouchDB obj
+    :param doc: document
+    :param field: field names
+    :param subfield: one level deep, for now. intended for coverage stats
+    :return:
+    """
+    try:
+        if subfield:
+            doc[field].pop(subfield, None)
+        else:
+            doc.pop(ast.literal_eval(field), None)
+        db.save_doc(doc)
+    except Exception, e:
+        print >>sys.stderr, "Error occurred when deleting field {} " + \
+                            "from doc: {}".format(field, json.dumps(doc))
+        if subfield:
+            print >>sys.stderr, "Subfield detected: {}".format(subfield)
+        raise
+
+
+def add_field_payload(db, doc, field, payload, is_coverage=False):
+    """
+    Add dict payload to new field in the given document
+    :param db: couchdb DB object
+    :param doc: document
+    :param field: new field name
+    :param payload: dict payload
+    :param is_coverage: boolean, is this coverage data?
+    :return: None
+    """
+    try:
+        if is_coverage:
+            if 'coverage' not in doc:
+                doc['coverage'] = {}
+            doc['coverage'][field] = ast.literal_eval(payload)
+        else:
+            doc[field] = ast.literal_eval(payload)
+        db.save_doc(doc)
+    except Exception, e:
+        print >>sys.stderr, "Error occurred when adding payload: {} to field: " + \
+                            "{} to doc: {}".format(
+            json.dumps(payload), field, json.dumps(doc))
+        print >>sys.stderr, "Coverage data: {}".format(is_coverage)
+        raise
+
+
 def main():
     args = docopt(__doc__)
     bnid = args['-b']
@@ -83,17 +157,17 @@ def main():
     credentials = args['-c']
 
     print "Connect to db: {} on server: {}".format(db_name, server_name)
-    server = connect_to_db(server_name, credentials)
+    server = connect_to_server(server_name, credentials)
     db = server.get_db(db_name)
 
     print "Searching for document with BNid: {}".format(bnid)
     rows = view_docs_by_bnid(db, bnid)
 
     # search through and remove all documents that appear to be duplicates
-    all_docs = db.view('igsb_qc/all', descending=True)
-    duplicate_docs = find_duplicate_docs(all_docs, db)
-    for d in duplicate_docs:
-        delete_document(db, d)
+    #all_docs = db.view('igsb_qc/all', descending=True)
+    #duplicate_docs = find_duplicate_docs(all_docs, db)
+    #for d in duplicate_docs:
+    #    delete_document(db, d)
 
     #delete_fields = args['-D']
     #print "Delete these fields: {}".format(delete_fields)
